190fa9830c2c40c7b544d07c34ac27e792f95034
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2024-02-05 12:08:22 +0200

    tehty pre-asennus jutut

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { VitePWA } from "vite-plugin-pwa";
+import manifest from './manifest.json';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+            VitePWA({ manifest: manifest })],
 })
+
+
+
